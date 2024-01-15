eb3bd93b01f7e51a8e3a6691281ac9f5968eb296
EsquivelGarcia20 <112773644+EsquivelGarcia20@users.noreply.github.com>
2024-01-15 10:21:02 -0600

    Add files via upload
    Main de la Calculadora

--- Main.java
+++ Main.java
@@ -1,57 +1,74 @@
-package Mi_Tamagochi;
+package Practica;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Elementos criatura = new Elementos();
-
-        boolean validacion = true;
         Scanner leer = new Scanner(System.in);
 
-        System.out.println("*********************************************");
-        System.out.println("Bienvenidos, aqui puedes crear tu Tamagochi");
-        System.out.println("Ingresa nombre de tu Tamagochi: ");
-        String nombre = leer.nextLine();
-        criatura.setNombre(nombre);
-        System.out.println("*********************************************");
-        System.out.println("Su nombre es: " + criatura.getNombre());
-        criatura.estadoTamagochi();
-        System.out.println("*********************************************");
+        Suma operacion1 = new Suma();
+        Resta operacion2 = new Resta();
+        Division operacion3 = new Division();
+        Multiplicacion operacion4 = new Multiplicacion();
 
-        while (validacion) {
-            System.out.println("¿Qué deseas hacer?");
-            System.out.println("1.- Jugar");
-            System.out.println("2.- Bañarlo");
-            System.out.println("3.- Alimentarlo");
+        boolean bandera = true;
 
-            int seleccion = leer.nextInt();
+        while (bandera) {
 
-            switch (seleccion) {
+            System.out.println("=======================");
+            System.out.println("Calculadora");
+            System.out.println("Ingrese la operación que desea: ");
+            System.out.println("1.- Suma");
+            System.out.println("2.- Resta");
+            System.out.println("3.- División");
+            System.out.println("4.- Multiplicación");
+            System.out.println("5.- Salir");
+
+            int opcion = leer.nextInt();
+
+            float numero1;
+
+            switch (opcion) {
                 case 1:
-                    criatura.juegoCriatura();
-                    criatura.getVida();
-                    criatura.estadoTamagochi();
+                    System.out.println("=======================");
+                    System.out.println("Ingrese un número para sumar: ");
+                    System.out.println("=======================");
+                    numero1 = leer.nextFloat();
+                    operacion1.sumar(numero1);
+                    operacion1.mostrarResultado();
                     break;
                 case 2:
-                    criatura.banaCriatura();
-                    criatura.getVida();
-                    criatura.estadoTamagochi();
+                    System.out.println("=======================");
+                    System.out.println("Ingrese el número en la primer posición para restar: ");
+                    System.out.println("=======================");
+                    numero1 = leer.nextFloat();
+                    operacion2.restar(numero1);
+                    operacion2.mostrarResultado();
                     break;
-
                 case 3:
-                    criatura.comerCriatura();
-                    criatura.getVida();
-                    criatura.estadoTamagochi();
+                    System.out.println("=======================");
+                    System.out.println("Ingrese el número en la primer posición para dividir: ");
+                    System.out.println("=======================");
+                    numero1 = leer.nextFloat();
+                    operacion3.realizarDivision(numero1);
+                    operacion3.mostrarResultado();
                     break;
-
+                case 4:
+                    System.out.println("=======================");
+                    System.out.println("Ingrese un número para multiplicar: ");
+                    System.out.println("=======================");
+                    numero1 = leer.nextFloat();
+                    operacion4.realizarMultiplicar(numero1);
+                    operacion4.mostrarResultado();
+                    break;
+                case 5:
+                    bandera = false;
+                    break;
                 default:
-                    System.out.println("Valor invalido, vuelva a ingresar una opción del menú");
+                    System.out.println("Opción no válida. Por favor, elija nuevamente.");
                     break;
             }
         }
-        validacion = false;
     }
 }
